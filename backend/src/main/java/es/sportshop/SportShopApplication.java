826a461209e863bce1ef5db10588dcafcdd1de75
delsazz <sergio.delsazcastillo24@campusfp.es>
2026-04-15 18:33:59 +0200

    Se ha creado el archivo SportShopApplication.java

---
+++ backend/src/main/java/es/sportshop/SportShopApplication.java
@@ -0,0 +1,9 @@
+package es.sportshop;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SportShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SportShopApplication.class, args);
+    }
+}
