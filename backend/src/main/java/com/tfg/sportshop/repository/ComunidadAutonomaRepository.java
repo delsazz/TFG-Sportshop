0eebe2b358a66628ad6dab451279a4b2ea6e4a4c
delsazz <sergio.delsazcastillo24@campusfp.es>
2026-05-17 17:30:05 +0200

    Se ha corregido el nombre del archivo ComunidadAutonomaRepository (de ComunidadRepository a ComunidadAutonomaRepository)

--- backend/src/main/java/com/tfg/sportshop/repository/ComunidadAutonomaRepository.java
+++ backend/src/main/java/com/tfg/sportshop/repository/ComunidadAutonomaRepository.java
@@ -1,5 +1,5 @@
 package com.tfg.sportshop.repository;
 import com.tfg.sportshop.model.ComunidadAutonoma;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface ComunidadRepository extends JpaRepository<ComunidadAutonoma, Integer> {
+public interface ComunidadAutonomaRepository extends JpaRepository<ComunidadAutonoma, Integer> {
 }
